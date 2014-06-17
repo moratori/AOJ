229faf6530e3e37580a676eaf0252b6453f0909a
moratori <hoko1993@yahoo.co.jp>
2014-06-17 11:58:20 +0900

    update

--- Volume/volume11/1132-1141/OhgasFortune.py
+++ Volume/volume11/1132-1141/OhgasFortune.py
@@ -1,3 +1,22 @@
 
 
 
+import math
+
+
+def simulation(left,duration,torf,ratio,fee):
+    acc = 0
+    fee = int(fee)
+    for d in xrange(int(duration)):
+        if ( torf == 0 ):
+            acc  += int(math.floor(left * ratio))
+            left -= fee
+        else:
+            left = int(math.floor(left * ratio) + left - fee)
+    return left + acc
+
+
+for d1 in xrange(input()):
+    init_money = input()
+    duration   = input()
+    print max([apply((lambda x,y,z: simulation(init_money,duration,x,y,z)),map(float,raw_input().split())) for d2 in xrange(input())])
